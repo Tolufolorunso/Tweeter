dc31c5cf0c9cd1ae91bcf13181c19b2aeab555e5
Tolu Folorunso <tolufolorunso@yahoo.com>
2022-09-22 20:37:36 +0100

    Starting the nav

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,5 @@
 import Heading from './Heading';
 import Button from './Button';
+import Nav from './nav/Nav';
 
-export { Heading, Button };
+export { Heading, Button, Nav };
---
+++ client/src/components/nav/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavWrapper from './nav.styled';
+
+const Nav = () => {
+  return (
+    <NavWrapper>
+      <div className="container">Nav</div>
+    </NavWrapper>
+  );
+};
+
+export default Nav;
