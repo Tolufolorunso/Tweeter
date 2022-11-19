428bd7cdcbf365915ea4a75dc8bd3c3878e96001
tolufolorunso <tolufolorunso@yahoo.com>
2022-11-19 23:18:24 +0100

    updated

--- client/src/components/tweet/TweetHeader.js
+++ client/src/components/tweet/TweetHeader.js
@@ -2,12 +2,18 @@ import Avater from "../Avater";
 import Text from "../Text";
 import DefaultAvater from "../../assets/images/defaultAvater.png";
 import { BiImage } from "react-icons/bi";
+import { AiOutlinePushpin } from "react-icons/ai";
 // import { useState } from "react";
 
 const span = { color: "#BDBDBD", fontWeight: 500 };
 
-const TweetHeader = ({ AvaterImage, userInfo,handleClickTweetMore, deleteTweet,more}) => {
-
+const TweetHeader = ({
+  AvaterImage,
+  userInfo,
+  handleClickTweetMore,
+  deleteTweet,
+  more,
+}) => {
   return (
     <header className="header">
       <Avater src={!AvaterImage ? AvaterImage : DefaultAvater} alt="tweeter" />
@@ -18,13 +24,18 @@ const TweetHeader = ({ AvaterImage, userInfo,handleClickTweetMore, deleteTweet,m
       <div className="header__more">
         <BiImage className="image" onClick={handleClickTweetMore} />
       </div>
-      <div className={`tweet__more ${more ? 'active-more' : ''}`}>
+      <div className={`tweet__more ${more ? "active-more" : ""}`}>
         <button onClick={deleteTweet}>delete</button>
         <button>delete</button>
         <button>delete</button>
         <button>delete</button>
         <button>delete</button>
       </div>
+      <AiOutlinePushpin
+        onClick={() => {
+          console.log("pin");
+        }}
+      />
     </header>
   );
 };
